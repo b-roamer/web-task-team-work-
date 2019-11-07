bec25f53351c4dbb025665c95e6355bb1b636542
Almas016 <55187430+Almas016@users.noreply.github.com>
2019-11-07 18:43:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ for (var i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('done');}
+  }, false);   
