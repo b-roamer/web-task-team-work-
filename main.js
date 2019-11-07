5e64e3811555f9186d27746620c1add83b2b79b1
Almas016 <55187430+Almas016@users.noreply.github.com>
2019-11-07 18:43:28 +0600

    Create main.js
    Main javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+var close = document.getElementsByClassName("close");
+for (var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
