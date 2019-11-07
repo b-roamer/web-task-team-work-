f649d0689fc88d4e509cf93ea3b9d21e195d6e62
Olzhik01 <55508244+Olzhik01@users.noreply.github.com>
2019-11-07 18:36:01 +0600

    Update serverProject.js

--- serverProject.js
+++ serverProject.js
@@ -4,3 +4,15 @@ function getTasks(){
 		return response.json();
 	});
 }
+function createTasks(title){
+	let promise = fetch(`https://repetitora.net/api/JS/Tasks?widgetId=15571&title=${title}`,{
+		method :'POST',
+		headers: {
+   			 'Content-Type': 'application/json;charset=utf-8'
+  		},
+   		body: JSON.stringify(title)
+	})
+	return promise.then((response)=>{
+		return response.json();
+	})
+}
